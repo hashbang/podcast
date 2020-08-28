2ca4eb12499286cd18c1d7fae3f843fb68910893
RyanSquared <vandor2012@gmail.com>
2020-08-27 23:58:49 -0500

    .github: fixup Dockerfile
    Title: Yoshi's Test

--- .github/actions/generate-metadata/entrypoint.sh
+++ .github/actions/generate-metadata/entrypoint.sh
@@ -4,5 +4,5 @@ duration=$(ffprobe -i $EP_FILE -show_entries stream=duration | \
              head -n -1 | \
              tail -n +2 | \
              cut -d= -f2)
-echo "::set-output title=ep_duration::$(printf "%.2f" "${duration}")"
-echo "::set-output title=ep_size::$(wc -c $EP_FILE | awk '{ print $1 }')"
+echo "::set-output name=ep_duration::$(printf "%.2f" "${duration}")"
+echo "::set-output name=ep_size::$(wc -c $EP_FILE | awk '{ print $1 }')"
